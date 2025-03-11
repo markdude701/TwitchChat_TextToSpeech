aecc598d0ea4834b05499fe65aa539684d7ae04c
Mark Makowski <33070355+markdude701@users.noreply.github.com>
2025-03-11 07:02:50 -0400

    Included actual main code

---
+++ Twitch/twitch_data.py
@@ -0,0 +1,4 @@
+CLIENT_ID: str = "..." # The CLIENT ID from the Twitch Dev Console
+CLIENT_SECRET: str = "..." # The CLIENT SECRET from the Twitch Dev Console
+BOT_ID = "..."  # The Account ID of the bot user...
+OWNER_ID = "..."  # Your personal User ID..
